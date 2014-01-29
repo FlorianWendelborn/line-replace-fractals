b4224737ebfc472b2eeea5f28f90a3d5cc3db815
FlorianWendelborn <florianwendgithub@googlemail.com>
2014-01-30 00:30:55 +0100

    work in progress

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var canvas, ctx;
 var height, width;
 
 // degrees, x, y, length
-var data = [[0,-.5,0,1]];
+var data = [[15,0,0,1]];
 var pattern = [
-	[90  ,0  ,0  ,.5          ],
-	[-45 ,0  ,.5 ,Math.sqrt(2)/2],
+	[-90  ,0  ,0  ,.5          ],
+	[45 ,0  ,-.5 ,Math.sqrt(2)/2],
 	[0   ,.5 ,0  ,.5          ]
 ];
 var s = 100;
@@ -35,44 +35,49 @@ function y (v) {
 }
 
 function render () {
-	ctx.strokeStyle = 'rgba(255,0,0,0.5)';
-	ctx.beginPath();
+	ctx.strokeStyle = 'rgba(255,0,0,0.2)';
 	for (var i = 0; i < data.length; i++) {
 		var v = data[i];
-		ctx.moveTo(width/2+s*v[1],height/2+s*v[2]);
-		ctx.lineTo(width/2+s*x(v),height/2+s*y(v));
+		drawVector(v);
 	}
+}
+
+function drawVector (v) {
+	ctx.beginPath();
+	ctx.moveTo(width/2+s*v[1],height/2+s*v[2]);
+	ctx.lineTo(width/2+s*x(v),height/2+s*y(v));
 	ctx.stroke();
 }
 
 function iterate () {
 	for (var i = 0; i < data.length; i++) {
 		var o = data[i];
-
-		ctx.beginPath();
-		ctx.strokeStyle = 'rgba(0,255,0,0.5)';
+		
 		for (var j = 0 ; j < pattern.length; j++) {
 			var p = pattern[j];
 
-			var ini = [
-				o[0],
-				p[1],
-				p[2],
-				0
-			];
+			var scaled = [
+				p[0]+o[0],
+				0,
+				0,
+				p[3]*o[3]
+			]
 
-			var v = [
-				ini[0]+p[0],
-				x(p)-ini[1],
-				y(p)-ini[2],
-				Math.sqrt(Math.pow(x(p)+x(ini),2)+Math.pow(y(p)+y(ini),2))
-			];
+			var moved = [
+				scaled[0],
+				o[3]*p[1]*Math.sin((scaled[0])*Math.PI/180),
+				o[3]*p[2]*Math.sin((90-scaled[0])*Math.PI/180),
+				scaled[3]
+			]
 
-			console.log(p,v);
+			console.log(moved);
 
-			ctx.moveTo(width/2+s*v[1],height/2+s*v[2]);
-			ctx.lineTo(width/2+s*(v[1]+Math.cos(v[0]*Math.PI/180)*v[3]),height/2+s*(v[2]+Math.sin(v[0]*Math.PI/180)*v[3]));
+			ctx.strokeStyle = 'rgba(0,255,0,0.2)';
+			//drawVector(p);
+			ctx.strokeStyle = 'rgba(0,0,255,0.2)';
+			drawVector(scaled);// blue
+			ctx.strokeStyle = 'rgba(0,255,0,0.2)';
+			drawVector(moved);// green
 		}
-		ctx.stroke();
 	}
 }
