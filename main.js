58f3ce7218c76a72e4647cdbed19cefed9cd0ae2
FlorianWendelborn <florianwendgithub@googlemail.com>
2014-01-30 19:00:56 +0100

    added userinterface
    can now store multiple fractal patterns

--- main.js
+++ main.js
@@ -1,33 +1,148 @@
 var canvas, ctx;
 var height, width;
+var data = [];
+var rendering, cancel;
 
-// degrees, x, y, length
-var data = [[0,-.5,0,1]];
-//var pattern = [[-90,0,0,.5],[45,0,-.5,Math.sqrt(2)/2],[180,1,0,.5]];
-//var pattern = [[-45,0,0,Math.sqrt(.5)],[45,.5,-.5,Math.sqrt(.5)]];
-//var pattern = [[-45,0,0,Math.sqrt(.5)],[135,1,1,Math.sqrt(.5)]];
-//var pattern = [[-45,0,0,Math.sqrt(.5)],[-135,1,0,Math.sqrt(.5)]];
-//var pattern = [[0,0,0,0.25],[45,0.25,0,Math.sqrt(0.125)],[-45,0.5,0.25,Math.sqrt(0.125)],[180,1,0,0.25]];
-//var pattern = [[45,0,0,0.7],[0,1,0,0.5]];
-//var pattern = [[45,0,0,0.7],[45,1,0,0.5]];
-var pattern = [[0,0,0,1/3],[0,2/3,0,1/3]];
-var iterations = 10;
-var s = 450;
+var s = 400;
+var animate = 200;
+var iterations, pattern;
+
+var patterns = [{
+	name: '',
+	f: [[]]
+},{
+	name: 'dragon',
+	f: [[-45,0,0,Math.sqrt(.5)],[-135,1,0,Math.sqrt(.5)]],
+	i: 14
+},{
+	name: 'koch-curve',
+	f: [[0,0,0,1/3],[-60,1/3,0,1/3],[60,1/2,-Math.sqrt(1/12),1/3],[0,2/3,0,1/3]],
+	i: 7
+},{
+	name: 'sierpinski-triangle',
+	f: [[0,0,0,.5],[0,.25,Math.sqrt(2)/4,.5],[0,.5,0,.5]],
+	i: 10
+},{
+	name: 'self-avoiding-dragon',
+	f: [[-90,0,0,.5],[45,0,-.5,Math.sqrt(2)/2],[180,1,0,.5]],
+	i: 10
+},{
+	name: '2fractal',
+	f: [[0,0,0,.5],[0,.5,0,.5],[90,0,-.5,.5],[90,.5,-.25,.25]],
+	i: 6
+},{
+	name: 'buggy-koch',
+	f: [[0,0,0,1/3],[-60,0,0,Math.sqrt(2/9)],[60,0,1/3,Math.sqrt(2/9)],[0,2/3,0,1/3]],
+	i: 7
+},{
+	name: 'weird lines',
+	f: [[-45,0,0,Math.sqrt(.5)],[135,1,1,Math.sqrt(.5)]],
+	i: 16
+},{
+	name: 'ice',
+	f: [[90,0,0.5,0.7],[0,0,0,0.5]],
+	i: 14
+},{
+	name: 'fern',
+	f: [[45,0,0,0.7],[0,1,0,0.5]],
+	i: 15
+},{
+	name: 'fern2',
+	f: [[0,0,0,0.5],[45,.5,0,Math.sqrt(.5)]],
+	i: 13
+},{
+	name: 'tree',
+	f: [[-45,0,0,Math.sqrt(.5)],[45,.5,-.5,Math.sqrt(.5)]],
+	i: 13
+},{
+	name: 'brain',
+	f: [[90,0,0,.5],[0,0,.5,.5],[90,.5,.5,.5],[-45,0.5,1,Math.sqrt(.5)]],
+	i: 9
+},{
+	name: 'wtf',
+	f: [[0,0,0,Math.sqrt(.5)],[90,0,.5,.5],[135,0.5,0,Math.sqrt(2)/2]],
+	i: 9
+},{
+	name: 'split',
+	f: [[0,0,0,1],[0,0,1,1/3],[0,2/3,1,1/3]],
+	i: 10
+},{
+	name: 'holes',
+	f: [[30,0,0,Math.sqrt(.5)],[30,0,0.5,Math.sqrt(.5)],[30,0,0,Math.sqrt(2)/2]],
+	i: 10
+},{
+	name: 'twist',
+	f: [[45,0,0,Math.sqrt(.5)],[-90,.5,.5,.5],[-45,0.5,0,Math.sqrt(2)/2]],
+	i: 10
+},{
+	name: 'multidragons',
+	f: [[45,0,0,0.7],[45,1,0,0.5]],
+	i: 15
+},{
+	name: 'like-a-dragon',
+	f: [[45,0,0,Math.sqrt(.5)],[-90,.5,.5,.5],[45,0.5,0,Math.sqrt(2)/2]],
+	i: 11
+},{
+	name: 'blob',
+	f: [[45,0,0,Math.sqrt(.5)],[-90,.5,.5,.5],[135,0.5,0,Math.sqrt(2)/2]],
+	i: 10
+}];
 
 window.onload = function () {
 	canvas = document.getElementsByTagName('canvas')[0];
 	ctx = canvas.getContext('2d');
 
-	for (var n = 0; n < iterations; n++) {
-		iterate();
+	var select = document.getElementById('choose');
+
+	for (var i = 0; i < patterns.length; i++) {
+		var e = document.createElement('option');
+		e.appendChild(document.createTextNode(patterns[i].name));
+		e.value = i;
+		select.appendChild(e);
 	}
 
 	updateDimensions();
 }
 window.onresize = updateDimensions;
 
+function updateS () {
+	s = document.getElementById('s').value;
+}
+function updateT () {
+	animate = document.getElementById('t').value;
+}
+
+function chosen () {
+	var select = document.getElementById('choose');
+
+	pattern = patterns[select.value].f;
+	data = [[0,-.5,0,1]];
+	iterations = patterns[select.value].i;
+
+	alert(select.value);
+
+	if (animate) {
+		if (rendering) {
+			cancel = true;
+			setTimeout(function () {
+				chosen();
+			},20);
+		} else {
+			cancel = false;
+			ranim(0);
+		}
+	} else if(!animate) {
+		for (var n = 0; n < iterations; n++) {
+			iterate();
+		}
+		render();
+	} else {
+		render();
+	}
+}
+
 function updateDimensions () {
-	height = window.innerHeight;
+	height = window.innerHeight-40;
 	width = window.innerWidth;
 	canvas.height = height;
 	canvas.width = width;
@@ -41,6 +156,20 @@ function y (v) {
 	return v[2]+Math.sin(v[0]*Math.PI/180)*v[3];
 }
 
+function ranim (n) {
+	rendering = true;
+	iterate();
+	render();
+	if (n < iterations && !cancel) {
+		setTimeout(function () {
+			ranim(++n);
+		},animate);
+	} else {
+		rendering = false;
+		console.log('finish');
+	}
+}
+
 function render () {
 	ctx.strokeStyle = 'rgba(255,0,0,1)';
 	ctx.beginPath();
@@ -48,6 +177,7 @@ function render () {
 		var v = data[i];
 		drawVector(v);
 	}
+	ctx.clearRect(0,0,width,height);
 	ctx.stroke();
 }
 
